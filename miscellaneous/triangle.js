dcee00a385cd7dbbdbcb94118a7d4e78f06630d3
xuejian.xu <jearbilove@yeah.net>
2017-06-26 22:47:50 +0800

    add common

---
+++ miscellaneous/triangle.js
@@ -0,0 +1,15 @@
+/**
+ *  有n根棍子，棍子i的长度为a(i)。想要从中选出3根棍子组成周长尽可能长的三角形。请输出最大的周长，若无法组成三角形则输出0。
+ *
+ *  limits：
+ *  3 <= n <= 100
+ *  1 <= a(i) <= 10^6
+ */
+
+require('../common/base');
+
+var inputs = utils.createInputs({
+    length : 60
+});
+
+logger.arr(inputs, 5);
