94b8cffb058cff0307625a0d2f95dbbcfb8c660f
xuxuejian <jearbilove@yeah.net>
2017-07-25 14:27:42 +0800

    update

--- common/CircularQueue.js
+++ common/CircularQueue.js
@@ -1,5 +1,22 @@
 /**
- * @fileOverView: #
+ * @fileOverView: Circular Queue
  * @author: xuejian.xu
  * @date: 2017/7/25.
  */
+
+class CircularQueue {
+    constructor(arr){
+        this.arr = arr || [];
+        this.index = -1;
+    }
+
+    get length(){
+        return this.arr.length;
+    }
+
+    next(){
+        return this.arr[++this.index%this.length]
+    }
+}
+
+module.exports = CircularQueue;
